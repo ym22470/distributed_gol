e64dd3d0b90bd0804025a23f3ac96058501d1ebc
chongsheng_wang <ym22470@users.noreply.github.com>
2023-11-16 14:37:21 +0000

    implemented step3 of distributed part

--- gol/distributor.go
+++ gol/distributor.go
@@ -3,6 +3,7 @@ package gol
 import (
 	"fmt"
 	"net/rpc"
+	"os"
 )
 
 type distributorChannels struct {
@@ -20,6 +21,21 @@ func makeCall(client *rpc.Client, world [][]byte, p Params, c distributorChannel
 	response := new(Response)
 	// where is this ProcessGol called? in server?
 	client.Call(ProcessGol, request, response)
+	//send the content of world and receive on the other side(writePgm) concurrently
+	c.ioCommand <- ioOutput
+	if p.Turns == 0 {
+		c.ioFilename <- fmt.Sprintf("%dx%dx0", p.ImageHeight, p.ImageWidth)
+	} else if p.Threads == 1 {
+		c.ioFilename <- fmt.Sprintf("%dx%dx%d", p.ImageHeight, p.ImageWidth, p.Turns)
+	} else {
+		c.ioFilename <- fmt.Sprintf("%dx%dx%d-%d", p.ImageHeight, p.ImageWidth, p.Turns, p.Threads)
+	}
+	//send the completed world to ioOutput c
+	for i := 0; i < p.ImageWidth; i++ {
+		for j := 0; j < p.ImageHeight; j++ {
+			c.ioOutput <- response.World[i][j]
+		}
+	}
 	//report the final state of the world
 	c.events <- FinalTurnComplete{CompletedTurns: response.CompletedTurns, Alive: response.AliveCells}
 	// Make sure that the Io has finished any output before exiting.
@@ -35,9 +51,16 @@ func makeCall(client *rpc.Client, world [][]byte, p Params, c distributorChannel
 func distributor(p Params, c distributorChannels) {
 	c.ioCommand <- ioInput
 	c.ioFilename <- fmt.Sprintf("%vx%v", p.ImageHeight, p.ImageWidth)
-	server := "172.31.87.77:8030"
+	server := "127.0.0.1:8030"
+	//server := "54.234.177.143:8030"
 	//create a client that dials to the tcp port
-	client, _ := rpc.Dial("tcp", server)
+	client, err := rpc.Dial("tcp", server)
+	if err != nil {
+		fmt.Println("fialed to dial to server")
+		os.Exit(1)
+
+	}
+
 	//close dial when everything is excuted
 	defer client.Close()
 
